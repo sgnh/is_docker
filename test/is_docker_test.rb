44b579a7313bc45d619e5ec1dff0e1d3162f5856
Steffen Hansen <sgnh@users.noreply.github.com>
2020-05-26 16:09:54 +0200

    Add tests

--- test/is_docker_test.rb
+++ test/is_docker_test.rb
@@ -1,11 +1,35 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class IsDockerTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::IsDocker::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_that_it_returns_true_with_cgroup
+    stub = proc { |args| ['docker'] if args == '/proc/self/cgroup' }
+
+    File.stub(:readlines, stub) do
+      assert IsDocker.docker? == true
+    end
+  end
+
+  def test_that_it_returns_false_with_cgroup
+    stub = proc { |args| [''] if args == '/proc/self/cgroup' }
+
+    File.stub(:readlines, stub) do
+      assert IsDocker.docker? == false
+    end
+  end
+
+  def test_that_it_returns_false_without_cgroup
+    stub = proc { |args|
+      raise Errno::ENOENT if args == '/proc/self/cgroup'
+    }
+
+    File.stub(:readlines, stub) do
+      assert IsDocker.docker? == false
+    end
   end
 end
