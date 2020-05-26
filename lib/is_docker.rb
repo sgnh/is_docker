4c66033b03ba015a2e6c2c39366cdfd4db78ec29
Steffen Hansen <sgnh@users.noreply.github.com>
2020-05-26 16:17:04 +0200

    Remove redundant begin block

--- lib/is_docker.rb
+++ lib/is_docker.rb
@@ -4,10 +4,8 @@ require 'is_docker/version'
 
 module IsDocker
   def self.docker?
-    begin
-      !File.readlines('/proc/self/cgroup').grep(/docker/).empty?
-    rescue Errno::ENOENT
-      false
-    end
+    !File.readlines('/proc/self/cgroup').grep(/docker/).empty?
+  rescue Errno::ENOENT
+    false
   end
 end
